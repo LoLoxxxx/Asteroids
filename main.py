a118851fc5e7cb1735ac57e26fc5434a0760579d
LoLoxxxx <kajtuh98@gmail.com>
2025-03-18 20:39:18 +0100

    Update

--- main.py
+++ main.py
@@ -1,19 +1,67 @@
 import pygame
 from constants import *
+from player import *
+from asteroids import *
+from asteroidfield import *
+import sys
+
+
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    
+
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    Shot.containers = (updatable, drawable, shots)
+
+    asteroid_field = AsteroidField()
+
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+    
+
+
+
     while True:
-        screen.fill(color=(0,0,0))
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        screen.fill("black")
+        
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collide(shot):
+                    asteroid.kill()
+                    asteroid.split()
+                    shot.kill()
 
 
+        for entity in drawable:
+            entity.draw(screen)
+        updatable.update(dt)
 
+        for asteroid in asteroids:
+            if asteroid.collide(player):
+                print('Game Over!')
+                sys.exit()
+
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        
 
 
 
